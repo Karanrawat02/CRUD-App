0edf4600e8ae3c7e0bbc36f2aed5a1cf6dc1f9d5
Karan Rawat <rawatkaranb02@gmail.com>
2023-02-20 10:51:36 +0530

    CRUD App

---
+++ src/Components/Employees.js
@@ -0,0 +1,30 @@
+import Home from "./Home"; 
+const Employees =[
+    {
+        id:"1",
+        Name:"Karan Rawat",
+        Age:"30"
+    },
+    {
+        id:"2",
+        Name:"Arjun Rawat",
+        Age:"27"
+    },
+    {
+        id:"3",
+        Name:"Rahul Rawat",
+        Age:"30"
+    },
+    {
+        id:"4",
+        Name:"Harsh Rawat",
+        Age:"32"
+    },
+    {
+        id:"5",
+        Name:"Deppesh Rawat",
+        Age:"31"
+    }
+]
+
+export default Employees;
